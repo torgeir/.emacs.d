0bc8a8b7305b73ccd0350550bb1455c986b02049
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-06-07 15:50:57 +0200

    Cleanup js dupes.

--- settings/setup-langs.el
+++ settings/setup-langs.el
@@ -21,8 +21,6 @@
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
-(setq-default js2-indent-on-enter-key 1)
-
 ;; js2-mode steals TAB, let's steal it back for yasnippet
 (require 'yasnippet)
 (defun js2-tab-properly ()
@@ -34,77 +32,20 @@
           (back-to-indentation)))))
 
 (define-key js2-mode-map (kbd "TAB") 'js2-tab-properly)
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "M-j") 'nil))
 
 (setq-default js2-basic-offset 2)
 (setq js-indent-level 2)
-
 
 (setq js2-highlight-level 3)
-;; js
-(require 'js2-mode)
-(autoload 'js2-mode "js2-mode" nil t)
-(add-hook 'js-mode-hook 'js2-minor-mode)
-
-(add-hook 'js2-mode-hook 'turn-on-smartparens-mode)
-
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
-(add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
-
-(setq-default js2-indent-on-enter-key 1)
-
-;; js2-mode steals TAB, let's steal it back for yasnippet
-(require 'yasnippet)
-(defun js2-tab-properly ()
-  (interactive)
-  (let ((yas-fallback-behavior 'return-nil))
-    (unless (yas-expand)
-      (indent-for-tab-command)
-      (if (looking-back "^\s*")
-          (back-to-indentation)))))
-
-(define-key js2-mode-map (kbd "TAB") 'js2-tab-properly)
-
-(setq-default js2-basic-offset 2)
-(setq js-indent-level 2)
-
-
-(setq js2-highlight-level 3)
-;; js
-(require 'js2-mode)
-(autoload 'js2-mode "js2-mode" nil t)
-(add-hook 'js-mode-hook 'js2-minor-mode)
-
-(add-hook 'js2-mode-hook 'turn-on-smartparens-mode)
-
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
-(add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
-
-(setq-default js2-indent-on-enter-key 1)
-
-;; js2-mode steals TAB, let's steal it back for yasnippet
-(require 'yasnippet)
-(defun js2-tab-properly ()
-  (interactive)
-  (let ((yas-fallback-behavior 'return-nil))
-    (unless (yas-expand)
-      (indent-for-tab-command)
-      (if (looking-back "^\s*")
-          (back-to-indentation)))))
-
-(define-key js2-mode-map (kbd "TAB") 'js2-tab-properly)
-
-(setq-default js2-basic-offset 2)
-(setq js-indent-level 2)
-(setq js2-highlight-level 3) ;; highlight functions as well
 
 ;; html
 (require 'sgml-mode)
+
+;; reindent after deleting tag with C-c DEL
+(defadvice sgml-delete-tag (after reindent-buffer activate)
+  (cleanup-buffer))
 
 (require 'simplezen)
 (defun --setup-simplezen ()
