0da96fe679bc27661170da4428ef7fd1fca67e89
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-06-07 19:30:14 +0200

    Flycheck to eslint js (with jsx support, almost)

--- settings/setup-langs.el
+++ settings/setup-langs.el
@@ -9,17 +9,31 @@
 (add-hook 'css-mode-hook 'rainbow-mode)
 
 ;; js
+;; Let flycheck handle parse errors
+(setq-default js2-show-parse-errors nil)
+(setq-default js2-strict-missing-semi-warning nil)
+(setq-default js2-strict-trailing-comma-warning t) ;; jshint does not warn about this now for some reason
+
 (require 'js2-mode)
 (autoload 'js2-mode "js2-mode" nil t)
 (add-hook 'js-mode-hook 'js2-minor-mode)
-
 (add-hook 'js2-mode-hook 'turn-on-smartparens-mode)
+(add-hook 'js2-mode-hook (lambda () (flycheck-mode 1)))
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.eslintrc$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
+
+(require 'flycheck)
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                          '(json-jsonlist)))
 
 ;; js2-mode steals TAB, let's steal it back for yasnippet
 (require 'yasnippet)
@@ -36,9 +50,8 @@
   '(define-key js2-mode-map (kbd "M-j") 'nil))
 
 (setq-default js2-basic-offset 2)
-(setq js-indent-level 2)
-
 (setq js2-highlight-level 3)
+(setq js-indent-level 2)
 
 ;; html
 (require 'sgml-mode)
@@ -55,6 +68,10 @@
 (add-hook 'sgml-mode-hook '--setup-simplezen)
 
 (define-key html-mode-map (kbd "TAB") 'simplezen-expand-or-indent-for-tab)
+
+;; nxml
+
+(add-hook 'nxml-mode-hook (setq nxml-child-indent 4))
 
 ;; clojure
 (require 'clojure-mode)
