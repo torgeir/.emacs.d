aec2be5ecbf760e807faa822b6f6e6e3d862ead2
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-06-14 12:47:59 +0200

    Fix os x copy paste.

--- settings/mac.el
+++ settings/mac.el
@@ -31,4 +31,17 @@
 
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 
+;; clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 (provide 'mac)
