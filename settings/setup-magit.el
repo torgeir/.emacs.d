a2449c36d8a65c807272ac8e59d60de2a1fdb50e
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-06-14 15:20:37 +0200

    Magit in full screen.

--- settings/setup-magit.el
+++ settings/setup-magit.el
@@ -3,4 +3,18 @@
 (global-set-key (kbd "C-x m") 'magit-status)
 (autoload 'magit-status "magit")
 
+;; make magit run full screen
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 (provide 'setup-magit)
