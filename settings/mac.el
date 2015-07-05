c1c979a58f61533216779f9ce23e617920a54380
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-07-05 22:17:48 +0200

    Try to remove the mac pbcopy stuff, seem to make trouble for yankring.

--- settings/mac.el
+++ settings/mac.el
@@ -31,16 +31,16 @@
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 
 ;; clipboard
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
 
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+;; (setq interprogram-cut-function 'paste-to-osx)
+;; (setq interprogram-paste-function 'copy-from-osx)
 
 (provide 'mac)
