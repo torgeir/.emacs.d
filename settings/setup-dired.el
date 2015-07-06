a4ea135c13af8061b72f1013aa576f67cf761dea
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-07-06 19:04:39 +0200

    Smarter movement in dired.

--- settings/setup-dired.el
+++ settings/setup-dired.el
@@ -7,7 +7,9 @@
 (global-set-key (kbd "C-x M-j") '(lambda () (interactive) (dired-jump 1)))
 
 (define-key dired-mode-map (kbd "M-<up>") (lambda () (interactive) (find-alternate-file "..")))
+(define-key dired-mode-map (kbd "M-p") (lambda () (interactive) (find-alternate-file "..")))
 (define-key dired-mode-map (kbd "M-<down>") (lambda () (interactive) (dired-find-alternate-file)))
+(define-key dired-mode-map (kbd "M-n") (lambda () (interactive) (dired-find-alternate-file)))
 
 ;; Make dired less verbose
 (require 'dired-details)
