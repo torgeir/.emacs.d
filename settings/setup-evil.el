9fc943dfe54b1fd4bc015f6e8cfada53e791b233
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-07-28 22:13:26 +0200

    Don't evil mode by default.

--- settings/setup-evil.el
+++ settings/setup-evil.el
@@ -1,11 +1,6 @@
 (require 'evil)
 
-(setq evil-search-module 'evil-search
-      evil-want-C-u-scroll t
-      evil-want-C-w-in-emacs-state t)
-
-(evil-mode t)
 (setq evil-default-state 'emacs)
-(evil-emacs-state t)
+(evil-mode t)
 
 (provide 'setup-evil)
