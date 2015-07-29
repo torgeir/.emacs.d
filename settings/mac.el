87ed087668e8d42a65f571b5204a43e3347b6664
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-07-29 21:53:37 +0200

    Copy also copies to os x.

--- settings/mac.el
+++ settings/mac.el
@@ -31,16 +31,16 @@
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 
 ;; clipboard
-;; (defun copy-from-osx ()
+(defun copy-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'copy-to-osx)
+
+;; (defun paste-from-osx ()
 ;;   (shell-command-to-string "pbpaste"))
-
-;; (defun paste-to-osx (text &optional push)
-;;   (let ((process-connection-type nil))
-;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-;;       (process-send-string proc text)
-;;       (process-send-eof proc))))
-
-;; (setq interprogram-cut-function 'paste-to-osx)
-;; (setq interprogram-paste-function 'copy-from-osx)
+;; (setq interprogram-paste-function 'paste-from-osx)
 
 (provide 'mac)
