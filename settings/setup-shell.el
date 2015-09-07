2d386daa6caa848aab2bffad30c60f51eb98060b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-09-07 16:53:19 +0200

    Tab complete in ansi-term. c-d to exit.

--- settings/setup-shell.el
+++ settings/setup-shell.el
@@ -10,6 +10,9 @@
 (require 'shell-command)
 (shell-command-completion-mode 1)
 
+;; fix tab-completion in ansi-term
+(add-hook 'term-mode-hook (lambda () (setq yas-dont-activate t)))
+
 ;; C-d to kill buffer if process is dead
 (defun comint-delchar-or-eof-or-kill-buffer (arg)
   (interactive "p")
@@ -21,4 +24,13 @@
           (lambda ()
             (define-key shell-mode-map (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
 
+(defun ansi-term-handle-close ()
+  "Close current term buffer when `exit' or c-d from term buffer."
+  (when (ignore-errors (get-buffer-process (current-buffer)))
+    (set-process-sentinel (get-buffer-process (current-buffer))
+                          (lambda (proc change)
+                            (when (string-match "\\(finished\\|exited\\)" change)
+                              (kill-buffer (process-buffer proc))
+                              (delete-window))))))
+(add-hook 'term-mode-hook 'ansi-term-handle-close)
 (provide 'setup-shell)
