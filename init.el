1b7a08eb65c76f7d2ef382d4af4fb51541f34630
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-10-11 18:46:56 +0200

    Remove unused geeknote shortcuts

--- init.el
+++ init.el
@@ -498,13 +498,6 @@
   :init
   (setq geeknote-command "python /usr/local/bin/geeknote")
   :config
-  (global-set-key (kbd "C-c g c") 'geeknote-create)
-  (global-set-key (kbd "C-c g e") 'geeknote-edit)
-  (global-set-key (kbd "C-c g f") 'geeknote-find)
-  (global-set-key (kbd "C-c g s") 'geeknote-show)
-  (global-set-key (kbd "C-c g r") 'geeknote-remove)
-  (global-set-key (kbd "C-c g m") 'geeknote-move)
-
   (evil-leader/set-key
     "gnc" 'geeknote-create
     "gne" 'geeknote-edit
