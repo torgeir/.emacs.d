ef29ad65d7b64232056dd651d81f569128ce7fc9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-02-27 23:07:54 +0100

    Fix M-y binding

--- setup/setup-evil.el
+++ setup/setup-evil.el
@@ -134,8 +134,8 @@
 (bind-key "C-y" 'evil-paste-pop evil-normal-state-map)
 (bind-key "C-y" 'evil-paste-pop evil-visual-state-map)
 ;; show kill ring when not in insert mode, where c-y repeats text from above line
-(bind-key "\M-y" 'helm-show-kill-ring evil-normal-state-map)
-(bind-key "\M-y" 'helm-show-kill-ring evil-visual-state-map)
+(bind-key "M-y" 'helm-show-kill-ring evil-normal-state-map)
+(bind-key "M-y" 'helm-show-kill-ring evil-visual-state-map)
 
 ;;; esc ought to quit
 (bind-key [escape] 'keyboard-quit evil-normal-state-map)
