36cf5cbf2d73c62a1612e10e43ec67e1b8101071
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-05-05 13:09:58 +0200

    Show inline evaled elisp at point

--- setup/langs.el
+++ setup/langs.el
@@ -283,7 +283,18 @@
 
   ;; match camel-case tokens
   (add-hook 'clojure-mode-hook 'subword-mode)
-  (add-hook 'clojure-mode-hook 'enable-paredit-mode))
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+  ;; inline eval vars in elisp
+  (autoload 'cider--make-result-overlay "cider-overlays")
+  (defun endless/eval-overlay (value point)
+    (cider--make-result-overlay (format "%S" value)
+      :where point
+      :duration 'command)
+    value) ; preserve the return value
+  (advice-add 'eval-region :around (lambda (f beg end &rest r) (endless/eval-overlay (apply f beg end r) end)))
+  (advice-add 'eval-last-sexp :filter-return (lambda (r) (endless/eval-overlay r (point))))
+  (advice-add 'eval-defun :filter-return (lambda (r) (endless/eval-overlay r (save-excursion (end-of-defun) (point))))))
 
 ;; lisp
 (t/declare-prefix-for-mode 'lisp-interaction-mode "me" "Evaluate"
