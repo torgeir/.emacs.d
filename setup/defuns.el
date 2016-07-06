3dddf0374e94a9efed175017406427b434b0230c
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-06 19:16:51 +0200

    t/sudo-edit

--- setup/defuns.el
+++ setup/defuns.el
@@ -592,4 +592,16 @@ Including indent-buffer, which should not be called automatically on save."
                  (org-find-exact-headline-in-buffer headline))))
       (org-refile nil nil (list headline file nil pos))))
 
+(defun t/sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+   With a prefix ARG prompt for a file to visit.
+   Will also prompt for a file to visit if current
+   buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'defuns)
