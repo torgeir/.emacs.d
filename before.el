de620096e1f9f29189b33a2f605bc8fa243778f9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-07 23:56:32 +0200

    Empty before.el file

--- before.el
+++ before.el
@@ -1,7 +1,1 @@
 
-(setq url-proxy-services '(("http" . "proxy.vegvesen.no:8080")))
-
-(setq tern-command '("C:\\cygwin64\\home\\torgth\\datainn\\web\\node\\tern"))
-(setq flycheck-javascript-eslint-executable "C:\\cygwin64\\home\\torgth\\datainn\\web\\node\\eslint")
-
-(provide 'proxy)
