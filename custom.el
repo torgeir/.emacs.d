8ba82f3ab7c5de36feb7ef4aa8b6914c10835f74
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-07-10 18:53:38 +0200

    Remove customizations of linum mode and show-paren-match

--- custom.el
+++ custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:background "#2a2b2e" :foreground "#44505c"))))
  '(org-checkbox-statistics-todo ((t (:inherit org-todo))) t)
  '(org-todo ((t (:background "#32322c" :foreground "#dc752f" :weight bold))))
  '(rainbow-delimiters-unmatched-face ((t (:foreground "#e0211d" :overline nil :underline t))))
- '(show-paren-match ((t (:background "#444")))))
+ )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
