14edafeae7fffc2de46761df3729c920fa1f9988
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-08-11 23:07:49 +0200

    remove dup eshell-init

--- setup/t-shell.el
+++ setup/t-shell.el
@@ -69,14 +69,6 @@
           (eshell/cd path)
           (eshell-reset)))))
 
-(defun t/eshell-init ()
-  (setq-default eshell-history-size 10000
-                eshell-save-history-on-exit t
-                ;; announce terminal
-                eshell-term-name "eterm-color"))
-
-(t/eshell-init)
-
 (defun t/eshell-quit ()
   "Fake a terminal's C-d to quit eshell"
   (interactive)
