9d191467e6cd3a19084b5eafe3e543dd69e3ffab
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-12-18 11:37:48 +0100

    clean t-sane-defaults

--- setup/t-sane-defaults.el
+++ setup/t-sane-defaults.el
@@ -18,45 +18,55 @@
 (when is-mac (require 't-mac))
 (when is-ms (require 't-cygwin))
 
-;; cut copies to os
-;;(setq interprogram-cut-function #'t/copy-to-clipboard)
+;; utf-8 ffs
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(add-to-list 'file-coding-system-alist '("\\.org" . utf-8))
+(prefer-coding-system 'utf-8)
+(modify-coding-system-alist 'file "" 'utf-8)
 
-;; update screen immediately
-(setq redisplay-dont-pause t)
+(setq-default
+ redisplay-dont-pause t ; update screen immediately
+ x-underline-at-descent-line t ; draw underline lower
+ help-window-select 't ; focus help buffers
+ visible-bell t ; visible bell
+ ring-bell-function 'ignore ; no bell
+ compilation-scroll-output 'first-error ; scroll compilation to first error
+ window-combination-resize t ; resize proportionally
+ initial-major-mode 'text-mode ; load *scratch* in text-mode
+ initial-scratch-message nil ; clear *scratch* buffer
+ echo-keystrokes 0.001 ; show keystrokes
+ save-interprogram-paste-before-kill t ; clipboard contents into kill-ring before replace
+ font-lock-maximum-decoration t ; gaudiest possible look
+ truncate-partial-width-windows nil ; don't truncate lines
+ indicate-empty-lines nil ; don't show empty lines after buffer
+ indicate-buffer-boundaries nil ; don't show buffer start/end
+ frame-title-format "%b (%f)"; full path in titlebar
+ inhibit-startup-message t ; no splash
+ sentence-end-double-space nil ; one space between sentences
+ ad-redefinition-action 'accept ; silence useless warnings, e.g. ad-handle-definition: `find-tag-noselect' got redefined
+ fill-column 80 ; chars per line
+ gc-cons-threshold 20000000 ; more memory
+ indent-tabs-mode nil ; don't use tabs
+ tab-width 2 ; two spaces
+ cursor-in-non-selected-windows nil ; no cursor in other open windows
+ eval-expression-print-length nil ; no length limit when printing sexps in message buffer
+ eval-expression-print-level nil) ; no level limit when printing sexps in message buffer
+
+;; y or n will do
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; dont blink cursor
 (blink-cursor-mode -1)
 
-;; draw underline lower
-(setq x-underline-at-descent-line t)
-
-;; no bell
-(setq ring-bell-function 'ignore)
-
-;; visible bell
-(setq visible-bell t)
-
-;; focus help buffers
-(setq help-window-select 't)
-
-;; scroll compilation to first error
-(setq compilation-scroll-output 'first-error)
-
-;; resize windows proportionally
-(setq window-combination-resize t)
-
-;; start *scratch* in text mode, loads emacs faster, and make it empty
-(setq initial-major-mode 'text-mode
-      initial-scratch-message nil)
-
-;; clipboard contents into kill-ring before replace
-(setq save-interprogram-paste-before-kill t)
-
-;; show keystrokes
-(setq echo-keystrokes 0.01)
-
-;; gaudiest possible look
-(setq font-lock-maximum-decoration t)
+;; remove menus
+(when window-system
+  (tooltip-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1))
 
 ;; show active region
 (transient-mark-mode 0)
@@ -68,102 +78,8 @@
 (show-paren-mode t)
 (setq show-paren-delay 0)
 
-;; no splash
-(setq inhibit-startup-message t)
-
-;; remove menu
-(when window-system
-  (tooltip-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (menu-bar-mode -1))
-
-;; show empty lines after buffer end
-(set-default 'indicate-empty-lines t)
-
-;; full path in titlebar
-(setq-default frame-title-format "%b (%f)")
-
-;; highlight TODOs
-(defface todo-face '((t (:foreground "white" :background "green" :bold t)))
-  "Face used for highlighting todos" :group 'basic-faces)
-(add-hook 'prog-mode-hook (lambda () (font-lock-add-keywords nil '(("\\(TODO\\)" 1 'todo-face t)))))
-
-;; show trailing whitespace in red
-(--each '(emacs-lisp-mode-hook
-          clojure-mode-hook
-          js2-mode-hook)
-  (add-hook it (lambda () (setq show-trailing-whitespace t))))
-
-;; Answering just 'y' or 'n' will do
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Don't truncate lines
-(setq truncate-partial-width-windows nil)
-
 ;; remove selected text when typing
 (delete-selection-mode t)
-
-;; 80 char lines
-(setq fill-column 80)
-
-;; above what sizes can the window split
-(setq split-height-threshold 0
-      split-width-threshold 0
-      split-window-preferred-function #'t/split-window-sensibly)
-
-;; spaces
-(setq indent-tabs-mode nil
-      tab-width 2)
-
-;; more memory
-(setq gc-cons-threshold 20000000)
-
-;; temp files in..
-(setq backup-directory-alist `((".*" . ,(locate-user-emacs-file ".backups/"))))
-(setq auto-save-file-name-transforms `((".*" ,(locate-user-emacs-file ".auto-save-list/") t)))
-(setq auto-save-list-file-prefix (locate-user-emacs-file ".auto-save-list/"))
-(setq recentf-save-file (locate-user-emacs-file ".recentf"))
-(setq save-place-file (locate-user-emacs-file ".places"))
-(setq save-place-forget-unreadable-files nil)
-(setq create-lockfiles nil)
-(setq ido-save-directory-list-file (locate-user-emacs-file ".ido.last"))
-
-;; save more recent files
-(use-package recentf
-  :defer t
-  :init
-  (setq recentf-max-saved-items 1000
-        recentf-auto-cleanup 'never)
-  :config
-  (setq recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
-  (recentf-mode 1))
-
-;; from spacemacs
-;; Highlight and allow to open http link at point in programming buffers
-;; goto-address-prog-mode only highlights links in strings and comments
-(add-hook 'prog-mode-hook 'goto-address-prog-mode)
-;; Highlight and follow bug references in comments and strings
-(add-hook 'prog-mode-hook 'bug-reference-prog-mode)
-;; Support folding
-(add-hook 'prog-mode-hook 'hs-minor-mode)
-
-;; remove ellipsis from sexps in message buffer
-(setq eval-expression-print-length nil
-      eval-expression-print-level nil)
-
-;; no cursor in other open windows
-(setq cursor-in-non-selected-windows nil)
-
-(use-package winner
-  :ensure nil
-  :config (winner-mode 1))
-
-;; add dirs to buffer names when not unique
-(use-package uniquify
-  :ensure nil
-  :init
-  (setq uniquify-buffer-name-style 'forward))
 
 ;; mouse, with scroll
 (xterm-mouse-mode t)
@@ -176,19 +92,54 @@
   (bind-key [mouse-5] '(lambda () (interactive) (scroll-up 2)))
   (mouse-wheel-mode t))
 
-;; utf-8 ffs
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(add-to-list 'file-coding-system-alist '("\\.org" . utf-8))
-(prefer-coding-system 'utf-8)
-(modify-coding-system-alist 'file "" 'utf-8)
+;; above what sizes can the window split
+(setq split-height-threshold 0
+      split-width-threshold 0
+      split-window-preferred-function #'t/split-window-sensibly)
 
-;; one space between sentences
-(setq sentence-end-double-space nil)
+;; temp files in..
+(setq backup-directory-alist `((".*" . ,(locate-user-emacs-file ".backups/")))
+      auto-save-file-name-transforms `((".*" ,(locate-user-emacs-file ".auto-save-list/") t))
+      auto-save-list-file-prefix (locate-user-emacs-file ".auto-save-list/")
+      recentf-save-file (locate-user-emacs-file ".recentf")
+      save-place-file (locate-user-emacs-file ".places")
+      save-place-forget-unreadable-files nil
+      create-lockfiles nil
+      ido-save-directory-list-file (locate-user-emacs-file ".ido.last"))
 
-;; silence useless warnings, e.g. ad-handle-definition: `find-tag-noselect' got redefined
-(setq ad-redefinition-action 'accept)
+;; save more recent files
+(use-package recentf
+  :defer t
+  :init
+  (setq recentf-max-saved-items 1000
+        recentf-auto-cleanup 'never)
+  :config
+  (setq recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
+  (recentf-mode 1))
+
+(use-package winner
+  :ensure nil
+  :config (winner-mode 1))
+
+;; add dirs to buffer names when not unique
+(use-package uniquify
+  :ensure nil
+  :init
+  (setq uniquify-buffer-name-style 'forward))
+
+;; highlight todos
+(defface t-todo-face
+  '((t (:foreground "white" :background "green" :bold t)))
+  "Face used for highlighting todos" :group 'basic-faces)
+(add-hook 'prog-mode-hook (lambda () (font-lock-add-keywords nil '(("\\(TODO\\)" 1 't-todo-face t)))))
+
+;; from spacemacs
+;; Highlight and allow to open http link at point in programming buffers
+;; goto-address-prog-mode only highlights links in strings and comments
+(add-hook 'prog-mode-hook 'goto-address-prog-mode)
+;; Highlight and follow bug references in comments and strings
+(add-hook 'prog-mode-hook 'bug-reference-prog-mode)
+;; Support folding
+(add-hook 'prog-mode-hook 'hs-minor-mode)
 
 (provide 't-sane-defaults)
