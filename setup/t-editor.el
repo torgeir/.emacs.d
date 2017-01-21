109a6ba520f20ffbb0b336f697cbbcc14f7ee650
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-01-21 20:05:40 +0100

    elisp inline eval preview

--- setup/t-editor.el
+++ setup/t-editor.el
@@ -828,4 +828,16 @@
 (use-package selectric-mode ; lol
   :commands selectric-mode)
 
+(progn
+  ;; inline evaled results when in elisp using cider
+  (autoload 'cider--make-result-overlay "cider-overlays")
+  (defun endless/eval-overlay (value point)
+    (cider--make-result-overlay (format "%S" value)
+                                :where point
+                                :duration 'command)
+    value) ; preserve the return value
+  (advice-add 'eval-region :around (lambda (f beg end &rest r) (endless/eval-overlay (apply f beg end r) end)))
+  (advice-add 'eval-last-sexp :filter-return (lambda (r) (endless/eval-overlay r (point))))
+  (advice-add 'eval-defun :filter-return (lambda (r) (endless/eval-overlay r (save-excursion (end-of-defun) (point))))))
+
 (provide 't-editor)
