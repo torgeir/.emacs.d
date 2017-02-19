6181f62ff382403f90735faacbec61a66ffc7e4e
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-02-19 18:55:47 +0100

    remove f-sharp-mode, trend micro thinks its a virus.. make it shut up.

--- langs/t-lang-fsharp.el
+++ langs/t-lang-fsharp.el
@@ -1,7 +1,7 @@
-(use-package fsharp-mode
-  :mode "\\.fs[iylx]?$"
-  :config
-  (t/declare-prefix-for-mode 'fsharp-mode "me" "Evaluate"
-                             "r" 'fsharp-eval-region))
+;; (use-package fsharp-mode
+;;   :mode "\\.fs[iylx]?$"
+;;   :config
+;;   (t/declare-prefix-for-mode 'fsharp-mode "me" "Evaluate"
+;;                              "r" 'fsharp-eval-region))
 
 (provide 't-lang-fsharp)
