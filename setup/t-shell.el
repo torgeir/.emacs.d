d9c4f44cfe807d567866f40c6cb44aaa6780523b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-04-27 08:34:31 +0200

    eshell git completion

--- setup/t-shell.el
+++ setup/t-shell.el
@@ -242,6 +242,43 @@ PWD is not in a git repo (or the git command is not found)."
                               (propertize branch 'face 'font-lock-string-face)
                               (propertize " $" 'face (if (zerop eshell-last-command-status) 'font-lock-comment-face 'error))
                               (propertize " " 'face 'font-lock-preprocessor-face))))
-                (t/propertize-read-only prompt)))))))
+                (t/propertize-read-only prompt)))))
+
+    (progn
+      ;; eshell git completion
+
+      (defconst pcmpl-git-commands
+        '("add" "bisect" "branch" "checkout" "clone"
+          "commit" "diff" "fetch" "grep"
+          "init" "log" "merge" "mv" "pull" "push" "rebase"
+          "reset" "rm" "show" "status" "tag" )
+        "List of `git' commands")
+
+      (defvar pcmpl-git-ref-list-cmd "git for-each-ref refs/ --format='%(refname)'"
+        "The `git' command to run to get a list of refs")
+
+      (defun pcmpl-git-get-refs (type)
+        "Return a list of `git' refs filtered by TYPE"
+        (with-temp-buffer
+          (insert (shell-command-to-string pcmpl-git-ref-list-cmd))
+          (goto-char (point-min))
+          (let ((ref-list))
+            (while (re-search-forward (concat "^refs/" type "/\\(.+\\)$") nil t)
+              (add-to-list 'ref-list (match-string 1)))
+            ref-list)))
+
+      (defun pcomplete/git ()
+        "Completion for `git'"
+        ;; Completion for the command argument.
+        (pcomplete-here* pcmpl-git-commands)
+        ;; complete files/dirs forever if the command is `add' or `rm'
+        (cond
+         ((pcomplete-match (regexp-opt '("add" "rm")) 1)
+          (while (pcomplete-here (pcomplete-entries))))
+         ;; provide branch completion for the command `checkout'.
+         ((pcomplete-match "checkout" 1)
+          (pcomplete-here* (pcmpl-git-get-refs "heads")))
+         ((pcomplete-match "co" 1)
+          (pcomplete-here* (pcmpl-git-get-refs "heads"))))))))
 
 (provide 't-shell)
