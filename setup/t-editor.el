b3675c6a5b0e6037ac03165ef12ee61381d3aec5
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-05-02 23:37:55 +0200

    darker matching paren

--- setup/t-editor.el
+++ setup/t-editor.el
@@ -611,6 +611,7 @@
   :commands highlight-parentheses-mode
   :init
   (progn
+    (set-face-foreground 'show-paren-match-face (face-foreground 'font-lock-comment-face))
     (setq hl-paren-colors '("DeepPink1" "maroon1" "maroon2" "maroon3" "DeepPink3"
                             "DeepPink4" "maroon4" "VioletRed4"
                             "magenta1" "magenta2" "magenta3" "magenta4"
