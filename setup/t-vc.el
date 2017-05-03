e406b47baafce502a558fa508722277d00940162
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-05-03 12:27:03 +0200

    spacemacs useful buffers; *org agenda*, *magit

--- setup/t-vc.el
+++ setup/t-vc.el
@@ -60,6 +60,9 @@
 
     (add-hook 'magit-log-mode-hook 'visual-line-mode)
 
+    (when (boundp 'spacemacs-useful-buffers-regexp)
+      (add-to-list 'spacemacs-useful-buffers-regexp "\\*magit.*"))
+
     (defadvice magit-blame-mode (after switch-to-emacs-mode activate)
       (if magit-blame-mode
           (evil-emacs-state 1)
