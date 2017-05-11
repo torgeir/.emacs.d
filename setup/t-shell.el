3443f55b39c7399da03b25e71fc09e4d8fff9b7f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-05-11 22:59:16 +0200

    improve git completion in eshell

--- setup/t-shell.el
+++ setup/t-shell.el
@@ -253,39 +253,42 @@ PWD is not in a git repo (or the git command is not found)."
       ;; eshell git completion
 
       (defconst pcmpl-git-commands
-        '("add" "bisect" "branch" "checkout" "clone"
+        '("pr"
+          "add" "bisect" "branch" "checkout" "clone"
           "commit" "diff" "fetch" "grep"
           "init" "log" "merge" "mv" "pull" "push" "rebase"
           "reset" "rm" "show" "status" "tag" )
         "List of `git' commands")
 
+      (defun pcmpl-git-remotes ()
+        "Return list of `git' remotes."
+        (-drop-last 1 (s-split "\r?\n" (shell-command-to-string "git remote show"))))
+
       (defvar pcmpl-git-ref-list-cmd "git for-each-ref refs/ --format='%(refname)'"
         "The `git' command to run to get a list of refs")
 
-      (defun pcmpl-git-get-refs (type)
-        "Return a list of `git' refs filtered by TYPE"
+      (defun pcmpl-git-get-refs (types)
+        "Return a list of `git' refs filtered by TYPE."
         (with-temp-buffer
           (insert (shell-command-to-string pcmpl-git-ref-list-cmd))
           (goto-char (point-min))
           (let ((ref-list))
-            (while (re-search-forward (concat "^refs/" type "/\\(.+\\)$") nil t)
-              (add-to-list 'ref-list (match-string 1)))
+            (dolist (type types)
+              (while (re-search-forward (concat "^refs/" type "/\\(.+\\)$") nil t)
+                (add-to-list 'ref-list (match-string 1))))
             ref-list)))
 
       (defun pcomplete/git ()
-        "Completion for `git'"
-        ;; Completion for the command argument.
+        "Completion for `git'."
+
         (pcomplete-here* pcmpl-git-commands)
-        ;; complete files/dirs forever if the command is `add' or `rm'
         (cond
+         ;; complete files/dirs forever if the command is `add' or `rm'
          ((pcomplete-match (regexp-opt '("add" "rm")) 1)
           (while (pcomplete-here (pcomplete-entries))))
+         ((pcomplete-match (regexp-opt '("pr")) 1)
+          (while (pcomplete-here (append (pcmpl-git-get-refs '("heads")) (pcmpl-git-remotes)))))
          ;; provide branch completion for the command `checkout'.
-         ((pcomplete-match "origin" 1)
-          (pcomplete-here* (pcmpl-git-get-refs "heads")))
-         ((pcomplete-match "checkout" 1)
-          (pcomplete-here* (pcmpl-git-get-refs "heads")))
-         ((pcomplete-match "co" 1)
-          (pcomplete-here* (pcmpl-git-get-refs "heads"))))))))
-
+         ((pcomplete-match "\\(co\\|checkout\\|merge\\|branch\\|diff\\)" 1)
+          (pcomplete-here* (pcmpl-git-get-refs '("heads")))))))))
 (provide 't-shell)
