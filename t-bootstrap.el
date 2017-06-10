c558096f32e3f2c986d8d3fa67de2aa4a4a5cc45
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-06-11 00:15:58 +0200

    larger default font on os x

--- t-bootstrap.el
+++ t-bootstrap.el
@@ -7,7 +7,7 @@
 
 (defconst t-dir-site-lisp (t/user-emacs-file "site-lisp"))
 (defconst t-file-autoloads (t/user-emacs-file "setup/autoloads.el"))
-(defconst t-font-size 17)
+(defconst t-font-size 22)
 
 (defconst is-mac (equal system-type 'darwin))
 (defconst is-cygwin (equal system-type 'cygwin))
