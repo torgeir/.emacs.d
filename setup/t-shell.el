a07232ac04e63ee196e92e87e83e159ac67c11d6
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-06-24 08:59:03 +0200

    gadd origin pr remote

--- setup/t-shell.el
+++ setup/t-shell.el
@@ -92,6 +92,7 @@
                       '("gds" "magit-diff-staged")
                       '("grep" "grep --color=always $*")
                       '("gs" "magit-status")
+                      '("gadd-origin-pr" "git config --add remote.origin.fetch \"+refs/pull/*/head:refs/remotes/origin/pr/*\"")
                       '("hidedesktop" "defaults write com.apple.finder CreateDesktop -bool false && killall Finder")
                       '("hidehidden" "defaults write com.apple.finder AppleShowAllFiles -boolean false && killall Finder")
                       '("ip" "dig +short myip.opendns.com @resolver1.opendns.com")
