97bef37f0846657f1fe73ae67e8e0b834da1660f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-06-24 08:51:31 +0200

    dim hl bg

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-symbol-face ((t (:background "gray22"))))
  '(org-checkbox-statistics-todo ((t (:inherit org-todo))))
  '(org-code ((t (:foreground "VioletRed1"))))
  '(org-date ((t (:foreground "turquoise2"))))
