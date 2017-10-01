31933d3b235c13ba51db737da5f349e593808248
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-10-01 20:28:08 +0200

    remove unused org-pairs

--- setup/t-org.el
+++ setup/t-org.el
@@ -562,10 +562,7 @@ Locally redefines org-agenda-files not to export all agenda files."
 ;; smartparens helpers
 (with-eval-after-load 'smartparens
   (sp-with-modes 'org-mode
-
     (sp-local-pair "*" "*" :actions '(insert wrap) :unless '(sp-point-after-word-p sp-point-at-bol-p) :wrap "C-*" :skip-match 'sp--org-skip-asterisk)
-    (sp-local-pair "_" "_" :unless '(sp-point-after-word-p) :wrap "C-_")
-    ;;(sp-local-pair "/" "/" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
     (sp-local-pair "~" "~" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
     (sp-local-pair "=" "=" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
     (sp-local-pair "«" "»")))
