3e7156be35ba681bf72ca635e0e18592ba69d0e9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2018-02-05 00:01:28 +0100

    helm-swoop-speed

--- setup/t-editor.el
+++ setup/t-editor.el
@@ -639,7 +639,7 @@
   :commands helm-swoop
   :init
   (setq helm-swoop-use-line-number-face t
-        helm-swoop-speed-or-color t)
+        helm-swoop-speed-or-color nil)
   :config
   (t/bind-in 'helm-swoop-edit-map
              "C-c C-c" 'helm-swoop--edit-complete
