26f7e72d1896edc0a62b5517a095ff203023fe49
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2018-04-06 07:44:38 +0200

    try without the modeline/frametitle

--- setup/t-modeline.el
+++ setup/t-modeline.el
@@ -1,14 +1,30 @@
 ;;; -*- lexical-binding: t; -*-
 (defun t-modeline/config ()
 
+  (progn ; try without the modeline and the frame-title
+
+    (setq t-old-modeline nil
+          t-old-frametitle nil)
+
+    (defun t/clean-frame ()
+      "Remove mode-line and frame-title for a cleaner look."
+      (interactive)
+      (if t-old-modeline
+          (progn
+            (setq-default mode-line-format t-old-modeline frame-title-format t-old-frametitle)
+            (setq         mode-line-format t-old-modeline frame-title-format t-old-frametitle)
+            (setq t-old-modeline nil t-old-frametitle nil))
+        (progn
+          (setq t-old-modeline mode-line-format t-old-frametitle frame-title-format)
+          (setq-default mode-line-format nil frame-title-format " ")
+          (setq         mode-line-format nil frame-title-format " ")))))
+
   (defun t/init-modeline ()
-    ;; (+doom-modeline|init)
-    ;; try without'em
-    (progn
-      (setq-default mode-line-format nil
-                    frame-title-format " ")
-      (setq mode-line-format nil
-            frame-title-format " ")))
+    (when (not (or (equal frame-title-format "")
+                   (equal frame-title-format " ")))
+      (+doom-modeline|init)
+      (when (not t-old-modeline) ;; first time around
+        (t/clean-frame))))
 
   (t/add-hook 'after-init-hook #'t/init-modeline)
 
