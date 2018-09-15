0bb09b8ef86402348436d46bdfcb3431de58f74d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2018-09-15 09:29:33 +0200

    setq dont toggle when t-debug-init

--- t-bootstrap.el
+++ t-bootstrap.el
@@ -21,7 +21,7 @@
 (defvar *t-indent* 2)
 (defvar *t-indent-xml* 4)
 (defvar *t-debug-init* nil "Debug/time startup")
-(when *t-debug-init* (toggle-debug-on-error))
+(when *t-debug-init* (setq debug-on-error t))
 
 (add-to-list 'load-path t-dir-setup)
 (add-to-list 'load-path t-dir-langs)
