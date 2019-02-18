261b07378e0b7a6f7feddc86a2bec22745b4be47
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2019-02-18 09:02:48 +0100

    cal holidays and json reformat

--- setup/langs/t-lang-json.el
+++ setup/langs/t-lang-json.el
@@ -3,6 +3,7 @@
   :mode "\\(json\\|jshintrc\\|eslintrc\\)$")
 
 (t/use-package json-reformat
-  :commands json-reformat)
+  :commands json-reformat
+  :init (setq json-reformat:indent-width 2))
 
 (provide 't-lang-json)
