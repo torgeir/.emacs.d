8d081f40f0fc98054bf21f8ed6ddec850615bcff
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2019-09-01 16:15:02 +0200

    t-desktop survive no project

--- setup/t-desktop.el
+++ setup/t-desktop.el
@@ -7,20 +7,23 @@
 
 (defun t/desktop-save ()
   (interactive)
-  (when-let ((desktop-base-file-name (t/desktop-project-name)))
-    (desktop-save (or desktop-dirname
-                      (t/project-root)) t)))
+  (if (t/desktop-project-name)
+      (desktop-save (or desktop-dirname
+                        (t/project-root)) t)
+    (message "Not in a project.")))
 
 
 (defun t/desktop-restore ()
   (interactive)
-  (if (file-exists-p (expand-file-name (t/desktop-project-name)))
-      (when-let ((desktop-base-file-name (t/desktop-project-name))
-                 (desktop-dirname (expand-file-name user-emacs-directory)))
-        (desktop-revert))
-    (progn
-      (dired ".")
-      (t/desktop-save))))
+  (if-let (project-name (t/desktop-project-name))
+      (if (file-exists-p (expand-file-name project-name))
+          (when-let ((desktop-base-file-name project-name)
+                     (desktop-dirname (expand-file-name user-emacs-directory)))
+            (desktop-revert))
+        (progn
+          (dired ".")
+          (t/desktop-save)))
+    (message "Not in a project.")))
 
 
 (provide 't-desktop)
