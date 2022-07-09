bd07a6dd4a803704cd417fff9baf03a771a77cc4
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-07-09 12:55:52 +0200

    s-return terminal

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(setenv "INSIDE_EMACS" "1")
+
 ;; This file controls what Doom modules are enabled and what order they load
 ;; in. Remember to run 'doom sync' after modifying it!
 
