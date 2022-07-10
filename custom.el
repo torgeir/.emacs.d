2f3b1a697e13a3b56eac719e1f20b040285e4aab
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-07-10 23:34:50 +0200

    doom-one theme

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
  '(twittering-edit-skeleton 'inherit-any)
  '(twittering-initial-timeline-spec-string '(":mentions" ":home"))
  '(twittering-pop-to-buffer-function '+twitter-display-buffer-fn)
