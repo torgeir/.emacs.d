e1f2e7f35d9ad5a0c8ee9991df1e6e33edf3bf71
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-07-10 08:26:28 +0200

    hugo, twit, insert mode c-d

--- custom.el
+++ custom.el
@@ -4,7 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default)))
+   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(twittering-edit-skeleton 'inherit-any)
+ '(twittering-initial-timeline-spec-string '(":mentions" ":home"))
+ '(twittering-pop-to-buffer-function '+twitter-display-buffer-fn)
+ '(twittering-request-confirmation-on-posting t)
+ '(twittering-timeline-footer "")
+ '(twittering-timeline-header "")
+ '(twittering-use-master-password t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +hugo)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -191,7 +191,7 @@
        everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
+       twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
