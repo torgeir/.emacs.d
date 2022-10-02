9c9eb2a434a60106b67777ba6c6fbe8f212da7dd
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-10-02 16:45:40 +0200

    moar org flags

--- init.el
+++ init.el
@@ -159,7 +159,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +hugo)               ; organize your plain life in plain text
+       (org +hugo +dragndrop +gnuplot +jupyter)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
