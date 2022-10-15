a2d678065be1673a984c38166dc3564f36ba4fc3
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-10-15 13:01:44 +0200

    modeline icons, buffername + ignores

--- packages.el
+++ packages.el
@@ -5,3 +5,9 @@
 (package! focus)
 (package! spray)
 (package! dired-subtree)
+(package! blamer
+  :recipe (:host github :repo "artawower/blamer.el")
+  :pin "99b43779341af0d924bfe2a9103993a6b9e3d3b2")
+(package! org-appear
+  :recipe (:host github :repo "awth13/org-appear")
+  :pin "60ba267c5da336e75e603f8c7ab3f44e6f4e4dac")
