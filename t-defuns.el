a957dba84b65135226435fb885964f3f97705f82
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-11-29 21:58:08 +0100

    eval replace in clojure-mode as well

--- t-defuns.el
+++ t-defuns.el
@@ -217,12 +217,15 @@ Including indent-buffer, which should not be called automatically on save."
 (defun t/eval-and-replace ()
   "Evaluate and replace the preceding sexp with its value."
   (interactive)
-  (backward-kill-sexp)
-  (condition-case nil
-      (prin1 (eval (read (current-kill 0)))
-             (current-buffer))
-    (error (message "Invalid expression")
-           (insert (current-kill 0)))))
+  (if (eq major-mode 'clojure-mode)
+      (cider-eval-last-sexp-and-replace)
+      (progn
+        (backward-kill-sexp)
+        (condition-case nil
+            (prin1 (eval (read (current-kill 0)))
+                   (current-buffer))
+          (error (message "Invalid expression")
+                 (insert (current-kill 0)))))))
 
 ;; run it with M-x lorem
 (define-skeleton lorem
