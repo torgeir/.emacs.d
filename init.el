20f55adf8b4f71729f7cbbcbf06f33a7a0d7f83e
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-12-16 23:56:14 +0100

    bring back yas, remove doom-dashboard

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       ;; doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        (emoji +unicode)  ; 🙂
        hl-todo           ; highlight todo/fixme/note/deprecated/hack/review
@@ -67,7 +67,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       ;;snippets          ; my elves. They type so I don't have to
+       snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
