e9b0195ad0fd0a18877c015bd8990ef777d39370
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2022-12-16 23:52:52 +0100

    disable temple

--- packages.el
+++ packages.el
@@ -14,5 +14,5 @@
   :recipe (:host github :repo "awth13/org-appear")
   :pin "60ba267c5da336e75e603f8c7ab3f44e6f4e4dac")
 (package! remark-mode)
-(package! tempel)
+;; (package! tempel)
 (package! command-log-mode)
