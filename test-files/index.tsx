8f65ea1b39797876c5980abf84e03e2e82cde582
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-03-03 21:47:13 +0100

    test-file deps

--- test-files/index.tsx
+++ test-files/index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { renderToStaticMarkup } from "react-dom/server"
 
+//TODO try this
+//(setq +tree-sitter-hl-enabled-modes '(not web-mode typescript-tsx-mode))
+
 const App: React.FunctionComponent = () => <div>The app</div>
 
 process.stdout.write(renderToStaticMarkup(<App />))
