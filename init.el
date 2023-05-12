5df874a451e51e1c8082d7dd9ec520654951cac4
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-05-12 22:40:42 +0200

    magit +forge, bump some pkgs, mainly evil-collection for mu4e support

--- init.el
+++ init.el
@@ -106,7 +106,7 @@
        (lsp
         +eglot ; https://rgoswami.me/posts/emacs-lang-servers/
         )               ; M-x vscode
-       magit              ; a git porcelain for Emacs
+       (magit +forge)              ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
