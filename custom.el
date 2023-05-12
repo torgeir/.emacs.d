3c5afda75d275cebd1fa360373e4e9b73d743b8f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-05-12 20:30:55 +0200

    custom

---
+++ custom.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7cf720b5ecb20f43fbeef85380a5a7c44cfff091aa8c1b0265d0297ab3bd5d3f" "1c698719e9d4a63a25e97c59db57d104845b93152002bba2642fe4a850e931c4" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6d0be7d58f4169e34ea0f3aad456f0e4f8414430c612efe66ec6739ad5ee5e26" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" default)))
+(put 'narrow-to-region 'disabled nil)
