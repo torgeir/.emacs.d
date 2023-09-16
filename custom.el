e2b6b21df9c61c58dcfcacaffeb4380310b41da7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-09-16 11:40:57 +0200

    custom

--- custom.el
+++ custom.el
@@ -3,6 +3,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   '("7cf720b5ecb20f43fbeef85380a5a7c44cfff091aa8c1b0265d0297ab3bd5d3f" "1c698719e9d4a63a25e97c59db57d104845b93152002bba2642fe4a850e931c4" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6d0be7d58f4169e34ea0f3aad456f0e4f8414430c612efe66ec6739ad5ee5e26" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" default)))
+   '("7cf720b5ecb20f43fbeef85380a5a7c44cfff091aa8c1b0265d0297ab3bd5d3f" "1c698719e9d4a63a25e97c59db57d104845b93152002bba2642fe4a850e931c4" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6d0be7d58f4169e34ea0f3aad456f0e4f8414430c612efe66ec6739ad5ee5e26" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" default))
+ '(safe-local-variable-values '((org-log-done-with-time)))
+ '(warning-suppress-types '((org-element-cache) (defvaralias))))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(nav-flash-face ((t (:background "DeepPink2"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "DeepPink4" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-10-face ((t (:foreground "VioletRed2" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-11-face ((t (:foreground "VioletRed1" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "DeepPink3" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "DeepPink2" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "DeepPink1" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "maroon4" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "maroon3" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "maroon2" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "maroon1" :overline nil :underline nil))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "VioletRed3" :overline nil :underline nil))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "Red" :overline nil :underline nil))))
+ '(show-paren-match ((t (:background nil :foreground "yellow" :weight bold))))
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
