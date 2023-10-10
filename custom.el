d6ded834293473446d72cf1cbfa733bcc9e02730
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-10-10 22:42:29 +0200

    accepted themes

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   '("7cf720b5ecb20f43fbeef85380a5a7c44cfff091aa8c1b0265d0297ab3bd5d3f" "1c698719e9d4a63a25e97c59db57d104845b93152002bba2642fe4a850e931c4" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6d0be7d58f4169e34ea0f3aad456f0e4f8414430c612efe66ec6739ad5ee5e26" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" default))
+   '("a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "7cf720b5ecb20f43fbeef85380a5a7c44cfff091aa8c1b0265d0297ab3bd5d3f" "1c698719e9d4a63a25e97c59db57d104845b93152002bba2642fe4a850e931c4" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "6d0be7d58f4169e34ea0f3aad456f0e4f8414430c612efe66ec6739ad5ee5e26" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values '((org-log-done-with-time)))
  '(warning-suppress-types '((org-element-cache) (defvaralias))))
 (put 'narrow-to-region 'disabled nil)
