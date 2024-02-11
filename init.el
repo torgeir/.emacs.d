cc9d8780127bdb62d086ea1291f3661867b90372
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-02-11 07:53:00 +0100

    remove unmaintained non working gist module

--- init.el
+++ init.el
@@ -98,7 +98,6 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist              ; interacting with github gists
        (lookup           ; navigate your code and its documentation
         +offline
         +docsets
