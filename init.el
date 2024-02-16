7c741d8d5579bb609fd5270642e9cb6b35f9ea6d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-02-16 17:45:11 +0100

    fix deprecated IS-<OS> consts

--- init.el
+++ init.el
@@ -118,7 +118,7 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        (tty +osc)               ; improve the terminal Emacs experience
 
        :lang
