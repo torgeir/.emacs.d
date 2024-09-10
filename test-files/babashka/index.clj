fd2a42b14e531e962f57cd2abb76acde272e1f76
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-09-10 22:46:41 +0200

    test-files/babashka for fast clj experiments

---
+++ test-files/babashka/index.clj
@@ -0,0 +1,19 @@
+#!/usr/bin/env bb
+
+;; SPC m '
+;; cider-jack-in-clj
+
+(ns user
+  (:require[babashka.deps :as deps]))
+
+
+(deps/add-deps '{:deps {lambdaisland/deep-diff2 {:mvn/version "2.7.169"}}})
+(use 'lambdaisland.deep-diff2)
+(pretty-print (diff {:a 1 :b 2} {:a 1 :b (range 10)}))
+
+
+(defn read-json [file-path]
+  (cheshire.core/decode (->> file-path slurp) true))
+
+
+(->> [1 2 2 3 3 3 4 4 4 4] frequencies println)
