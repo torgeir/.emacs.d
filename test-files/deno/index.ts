88fac4ca8d2fbb1ba570ca6ce268290f6246ce06
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-07-12 16:48:38 +0200

    test-files deno tweaks

--- test-files/deno/index.ts
+++ test-files/deno/index.ts
@@ -6,6 +6,10 @@ const str = Deno.readFileSync("deno.json");
 const res = Deno.inspect(str);
 console.log(res);
 
+export function add(a: number, b: number) {
+  return a + b;
+}
+
 const a = 1;
 
 console.log(b);
--- test-files/deno/main_test.ts
+++ test-files/deno/main_test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts"
-import { add } from "./main.ts"
+import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
+import { add } from "./index.ts";
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5)
-})
+  assertEquals(add(2, 3), 5);
+});
